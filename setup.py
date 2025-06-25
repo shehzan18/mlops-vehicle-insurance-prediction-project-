b538d7922d8573515aa976cebfc7ff3e4c5c342e
Shehzan <shehzan1818@gmail.com>
2025-06-25 16:18:25 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup , find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author= "Shehzan",
+    author_email= "shehzan1818@gmail.com",
+    packages = find_packages()
+
+)
